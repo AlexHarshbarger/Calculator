53cf481f812165db59b5932f35f70f134d6d5595
Alex Harshbarger <harshbarger.a@gmail.com>
2022-04-04 12:38:42 -0400

    changed looked and trying different methods of saving variable

--- script.js
+++ script.js
@@ -1,44 +1,71 @@
-const btns = document.querySelectorAll('.btn')
-const plus = document.querySelector('.plus');
-const sub = document.querySelector('.sub')
-const multi = document.querySelector('.multi')
-const division = document.querySelector('.division')
-const equals = document.querySelector('.equals')
+const numbers = document.querySelectorAll('.numbers')
+const operators = document.querySelectorAll('.operator')
 const cleared = document.querySelector('.cleared')
 
 let firstNum = "";
 let secondNum = "";
-let operator = plus;
-let total = "";
+let operator = "+";
 
-btns.forEach(function(currentBtn){
+numbers.forEach(currentBtn => {
     currentBtn.addEventListener('click', function(e){
-        document.querySelector('.header').textContent += currentBtn.textContent;
-        firstNum = document.querySelector('.header').textContent
+        if (operator === "") {
+            document.querySelector('.header').textContent += e.target.innerText;
+            firstNum += e.target.innerText;
+            console.log(firstNum)
+        } else {
+            document.querySelector('.header').textContent += e.target.innerText;
+            secondNum += e.target.innerText;
+            console.log(secondNum)
+        }
     })
 })
 
-plus.addEventListener('click', function(){
-
-})
-
 function operate(firstNum, secondNum, operator) {
-    if (operator == plus) {
-        return parseInt(firstNum) + parseInt(secondNum);
-    } else if (operator == sub) {
-        return parseInt(firstNum) + parseInt(secondNum);
-    } else if (operator == multi) {
-        return parseInt(firstNum) + parseInt(secondNum);
-    } else if (operator == division) {
-        return parseInt(firstNum) + parseInt(secondNum);
-    }
+
 }
 
+
+operators.forEach(op => {
+    op.addEventListener('.click', e => {
+        if (e.target.innerText !== "=") {
+            operator = e.target.innerText;
+
+            console.log(firstNum)
+            console.log(operator)
+        } else {
+            switch(operator) {
+                case "+":
+                    document.querySelector('.header').textContent = parseInt(firstNum) + parseInt(secondNum);
+                    break;
+                
+                case "-":
+                    document.querySelector('.header').textContent = parseInt(firstNum) - parseInt(secondNum);
+                    break;
+                
+                case "*":
+                    document.querySelector('.header').textContent = parseInt(firstNum) * parseInt(secondNum);
+                    break;
+
+                case "/":
+                    document.querySelector('.header').textContent = parseInt(firstNum) / parseInt(secondNum);
+                    break;
+                
+                default:
+                    break;
+            }
+        }
+    })
+})
+
+
 cleared.addEventListener('click', function(){
+    firstNum = "";
+    secondNum = "";
+    operator = "";
     document.querySelector('.header').textContent = "";
 })
 
-equals.addEventListener('click', function(){
-    total = operate(firstNum, secondNum, operator)
-    document.querySelector('.header').textContent = total;
-})
+// equals.addEventListener('click', function(){
+//     total = operate(firstNum, secondNum, operator)
+//     document.querySelector('.header').textContent = total;
+// })
